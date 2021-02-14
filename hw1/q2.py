c00d3503a0ea66e68c021fbee9e7cb74c97b0280
drw <nedea.a@northeastern.edu>
2021-02-13 21:48:16 -0500

    refactored classification to take loss matrix

--- hw1/q2.py
+++ hw1/q2.py
@@ -18,6 +18,8 @@ PRIOR1=0.3
 PRIOR2=0.3
 PRIOR3=0.4
 
+PRIORS=[PRIOR1, PRIOR2, PRIOR3]
+
 # total no. of samples
 SAMPLE_NO=10000
 SAMPLE_NO_1=0 # samples for distribution 1
@@ -60,7 +62,7 @@ M1 = np.array([
     0.2,
     0.2,
     0.2
-])
+    ])
 
 dist1 = multivariate_normal(M1, C1)
 xl1 = dist1.rvs(size=SAMPLE_NO_1)
@@ -69,16 +71,16 @@ xl1 = dist1.rvs(size=SAMPLE_NO_1)
 # covariance matrix for class 2
 C2 = np.array([
     [4,       -0.2,    -0.1],
-    [-0.1,    4,        0],
+    [-0.1,    16,        0],
     [0,       0.12,     1]])
 
 # mean for class 2
 # for each dimension, it is 2*avg. stddev away from the previous class' mean for that dimension
 M2 = np.array([
     4.2, # 0.2 + 2*((sqrt(4)+sqrt(4))/2) = 0.2 + 2*2 = 4.2
-    4.2, # 0.2 + 2*((sqrt(4)+sqrt(4))/2) = 0.2 + 2*2 = 4.2
+    6.2, # 0.2 + 2*((sqrt(4)+sqrt(16))/2) = 0.2 + 2*3 = 6.2
     2.2  # 0.2 + 2*((sqrt(1)+sqrt(1))/2) = 0.2 + 2*1 = 2.2
-])
+    ])
 
 dist2 = multivariate_normal(M2, C2)
 xl2 = dist2.rvs(size=SAMPLE_NO_2)
@@ -94,9 +96,9 @@ C3 = np.array([
 # first mean for class 3
 M3 = np.array([
     8.2, # 4.2 + 2*((sqrt(4)+sqrt(4))/2)
-    8.2, # 4.2 + 2*((sqrt(4)+sqrt(4))/2)
+    12.2, # 12.2 + 2*((sqrt(16)+sqrt(4))/2)
     4.2  # 2.2 + 2*((sqrt(1)+sqrt(1))/2)
-])
+    ])
 
 # second covariance matrix for class 3
 C4 = np.array([
@@ -107,9 +109,9 @@ C4 = np.array([
 # second mean for class 3
 M4 = np.array([
     12.2, # 8.2 + 2*((sqrt(4)+sqrt(4))/2)
-    12.2, # 8.2 + 2*((sqrt(4)+sqrt(4))/2)
+    14.2, # 12.2 + 2*((sqrt(4)+sqrt(4))/2)
     6.2   # 4.2 + 2*((sqrt(1)+sqrt(1))/2)
-])
+    ])
 
 dist3_0 = multivariate_normal(M3, C3)
 xl3_0 = dist3_0.rvs(size=SAMPLE_NO_3)
@@ -163,20 +165,57 @@ for lv in labeled_v:
 classification: list[int] = []
 
 # Determine heuristic classifications
-for k in range(len(pxl1)):
-    # compute p(x|L=1)*p(L=1)
-    p1 = pxl1[k].value * PRIOR1
-    # compute p(x|L=2)*p(L=2)
-    p2 = pxl2[k].value * PRIOR2
-    # compute p(x|L=3)*p(L=3)
-    p3 = pxl3[k].value * PRIOR3
-    
-    if p1 >= p2 and p1 >= p3:
-        classification.append('1')
-    elif p2 >= p1 and p2 >= p3:
-        classification.append('2')
-    else:
-        classification.append('3')
+# TODO: Change this to use a loss matrix
+one_zero_loss_matrix = np.array(
+            [[0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0]]
+        )
+
+ten_loss_matrix = np.array(
+            [[0, 1, 10],
+            [1, 0, 10],
+            [1, 1, 0]]
+        )
+
+hundred_loss_matrix = np.array(
+            [[0, 1, 100],
+            [1, 0, 100],
+            [1, 1, 0]]
+        )
+
+print('shape of one zero loss matrix: ', one_zero_loss_matrix.shape)
+
+def classify(pxl, loss_matrix: np.array):
+    """
+    Classifies each X given a loss_matrix and P(L|X) for all L and X.
+
+    :param pxl: MxNxZ array; M = number of labels; N = number of samples; Z = dimension of sampled
+                vector X
+    :param loss_matrix: loss matrix
+    """
+    classification = []
+
+    print('given loss matrix:', loss_matrix)
+    # for each X to classify
+    for k in range(len(labeled_v)):
+        risks = [0, 0, 0]
+
+        # for each action alpha_i given X
+        for i in range(len(risks)):
+            # for each class L
+            for j in range(len(risks)):
+                prob = pxl[j][k].value
+                loss = loss_matrix[i][j]
+                risks[i] += loss * prob * PRIORS[j]
+
+        # choose action with least risk
+        decision = risks.index(min(risks)) + 1
+        classification.append(str(decision))
+
+    return classification
+
+classification = classify([pxl1, pxl2, pxl3], one_zero_loss_matrix)
 
 print('classification no.: ', len(classification))
 
@@ -185,13 +224,34 @@ ccl = defaultdict(list)
 # Initialize dict to hold incorrectly classified
 iccl = defaultdict(list)
 
+# Initialize confusion matrix
+confusion_matrix = np.zeros((3,3), dtype=float)
+
 # Determine correctly/incorrectly classified x
 for k in range(len(labeled_v)):
     label = labeled_v[k].label
+
+    # check if L=D (label=L, classification[k]=D)
     if label == classification[k]:
         ccl[label].append(labeled_v[k].value)
     else:
         iccl[label].append(labeled_v[k].value)
+    confusion_matrix[int(classification[k])-1][int(label)-1] += 1
+
+# Scale confusion matrix
+for k in range(len(confusion_matrix)):
+    for j in range(len(confusion_matrix[k])):
+        class_no_samples = 1
+        if j == 0:
+            class_no_samples = SAMPLE_NO_1
+        elif j == 1:
+            class_no_samples = SAMPLE_NO_2
+        else:
+            class_no_samples = SAMPLE_NO_3 + SAMPLE_NO_4
+
+        confusion_matrix[k][j] = confusion_matrix[k][j] / class_no_samples
+
+print('Confusion matrix:', confusion_matrix)
 
 # Convert lists to np.array
 for label in ccl:
